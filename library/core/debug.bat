3b8990c7f3ff8ae8bb154acf31c4bc02db161f95
gtinikita203 <gtinikita2031@proton.me>
2026-09-03 20:50:06 +0500

    Inject core.version=26.7.28 into libexclavecore to pass Xray minClientVer check on 3x-ui >= 3.5.0

--- library/core/debug.bat
+++ library/core/debug.bat
@@ -2,7 +2,7 @@
 
 set CGO_LDFLAGS=-Wl,-z,max-page-size=16384
 
-gomobile bind -v -androidapi 21 -tags="with_clash" "github.com/exclavenetwork/libexclavecore"
+gomobile bind -v -androidapi 21 -ldflags="-X github.com/exclavenetwork/exclave-core/v5/core.version=26.7.28 -X github.com/xtls/xray-core/core.version=26.7.28 -X github.com/sagernet/sing-box/constant.Version=26.7.28" -tags="with_clash" "github.com/exclavenetwork/libexclavecore"
 if errorlevel 1 (
     exit /b 1
 )

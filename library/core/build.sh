14b2091cb2a1ccd24e536eab19a4cf4c87267745
gtinikita203 <gtinikita2031@proton.me>
2026-09-03 21:21:55 +0500

    Inject comprehensive version package paths for v2ray-core and xray-core

--- library/core/build.sh
+++ library/core/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CGO_LDFLAGS="-Wl,-z,max-page-size=16384" gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -w -buildid= -X github.com/exclavenetwork/exclave-core/v5/core.version=26.7.28 -X github.com/xtls/xray-core/core.version=26.7.28 -X github.com/sagernet/sing-box/constant.Version=26.7.28" -tags="with_clash" "github.com/exclavenetwork/libexclavecore" || exit 1
+CGO_LDFLAGS="-Wl,-z,max-page-size=16384" gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -w -buildid= -X v2ray.com/core.version=26.7.28 -X github.com/v2fly/v2ray-core/v5.version=26.7.28 -X github.com/v2fly/v2ray-core/v4.version=26.7.28 -X github.com/v2fly/v2ray-core/v5/core.version=26.7.28 -X github.com/exclavenetwork/exclave-core/v5.version=26.7.28 -X github.com/exclavenetwork/exclave-core/v5/core.version=26.7.28 -X github.com/xtls/xray-core/core.version=26.7.28 -X github.com/sagernet/sing-box/constant.Version=26.7.28" -tags="with_clash" "github.com/exclavenetwork/libexclavecore" || exit 1
 
 proj=../../app/libs
 if [ -d $proj ]; then

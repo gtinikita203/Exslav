ca6635b8a2a58a78f4a472ff5df46f833d8eb4d9
gtinikita203 <gtinikita2031@proton.me>
2026-09-04 20:25:15 +0500

    Patch exclave-core REALITY SessionId version bytes from 25.5.16 to 26.7.28 during build

--- library/core/build.sh
+++ library/core/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+go mod download
+CORE_PATH=$(go list -m -f '{{.Dir}}' github.com/exclavenetwork/exclave-core/v5 2>/dev/null || true)
+if [ -n "$CORE_PATH" ] && [ -f "$CORE_PATH/transport/internet/reality/client.go" ]; then
+    chmod -R u+w "$CORE_PATH/transport/internet/reality"
+    sed -i 's/hello\.SessionId\[0\] = 25/hello.SessionId[0] = 26/g' "$CORE_PATH/transport/internet/reality/client.go"
+    sed -i 's/hello\.SessionId\[1\] = 5/hello.SessionId[1] = 7/g' "$CORE_PATH/transport/internet/reality/client.go"
+    sed -i 's/hello\.SessionId\[2\] = 16/hello.SessionId[2] = 28/g' "$CORE_PATH/transport/internet/reality/client.go"
+fi
+
 CGO_LDFLAGS="-Wl,-z,max-page-size=16384" gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -w -buildid= -X v2ray.com/core.version=26.7.28 -X github.com/v2fly/v2ray-core/v5.version=26.7.28 -X github.com/v2fly/v2ray-core/v4.version=26.7.28 -X github.com/v2fly/v2ray-core/v5/core.version=26.7.28 -X github.com/exclavenetwork/exclave-core/v5.version=26.7.28 -X github.com/exclavenetwork/exclave-core/v5/core.version=26.7.28 -X github.com/xtls/xray-core/core.version=26.7.28 -X github.com/sagernet/sing-box/constant.Version=26.7.28" -tags="with_clash" "github.com/exclavenetwork/libexclavecore" || exit 1
 
 proj=../../app/libs
